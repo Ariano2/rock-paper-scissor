227db2668a78f296820b82a8198775a0cd48914b
Ariano2 <ariano@Xubuntu.myguest.virtualbox.org>
2023-09-02 12:43:49 +0530

    Add a user interface to play the game

--- script.js
+++ script.js
@@ -5,26 +5,42 @@ let choice=Math.floor((Math.random()*100)%3);
 return (choiceList[choice]);
 }
 
-function getUserChoice(){
-    let userChoice=prompt("Enter your choice");
-    return userChoice.toLowerCase();
+function getUserChoice(event){
+    game((event.target.getAttribute('id')),getComputerChoice);
 }
 
-function inputValidator(){
-    let userChoice=getUserChoice();
-    for(let i=0;i<choiceList.length;i++){
-        if(choiceList[i]==userChoice){
-            return true;
-        }
-    }
-    return false;
+function finalWinnerDisplay(){
+    const finale=document.createElement('div');
+    const userScore=document.createElement('div');
+    const computerScore=document.createElement('div');
+    const winner=document.createElement('div');
+    userScore.innerText='User Wins : '+userWin;
+    computerScore.innerText='Computer Wins : '+(5-userWin);
+    if(userWin>(5-userWin)){winner.innerText="User Has Won";}
+    else if(userWin<(5-userWin)){winner.innerText="Computer Has Won";}
+    else{winner.innerText="Match Drawn";}
+    finale.appendChild(userScore);
+    finale.appendChild(computerScore);
+    finale.appendChild(winner);
+    const body=document.querySelector('body');
+    userScore.classList.add('box');
+    computerScore.classList.add('box');
+    winner.classList.add('box');
+    body.insertBefore(finale,resetBtn);
+    finale.classList.add('finale');
 }
 
 function game(userInput,computerInput,rounds){
-    let wins=0;
-    for(let i=0;i<rounds;i++){
+    if(roundCount>=5){
+        finalWinnerDisplay();
+        buttons.forEach((btn)=>btn.removeEventListener('click',getUserChoice));
+        let beat=new Audio("./audio/game-over.mp3");
+        beat.play();
+        return;
+    }
+    roundCount+=1;
         let result=2;
-        let userChoice=userInput();
+        let userChoice=userInput;
         let computerChoice=computerInput();
         switch(userChoice){
             case "rock":
@@ -70,20 +86,36 @@ function game(userInput,computerInput,rounds){
         }
         
         //proper output format
+        const output=document.querySelectorAll('.result > div > span');
+        output[0].innerText=userChoice;
+        output[1].innerText=computerChoice;
+        let message;
         console.log("User Chose : "+userChoice+"\n");
         console.log("Computer Chose: "+computerChoice+"\n");
         if(result===1){
-            console.log("User Won!\n");
-            wins+=1;
+            message="User Won!";
+            userWin+=1;
         }
         else if(result===0){
-            console.log("Computer Won!\n");
+            message="Computer Won!";
+    
         }
         else{
-            console.log("Stalemate\n");
+            message="Stalemate";
         }
-    }
-    console.log("\nTotal user wins are : "+wins+'\n');
+        output[2].innerText=message;
+    const rnd=document.querySelector('#round p');
+    rnd.innerText='Round : '+roundCount;
+    let beat=new Audio("./audio/coin.mp3");
+    beat.play();
 }
 
-game(getUserChoice,getComputerChoice,5)
+let roundCount=0;
+let userWin=0;
+const buttons=document.querySelectorAll('.gameChoice > button');
+buttons.forEach((btn)=>btn.addEventListener('click',getUserChoice));
+const resetBtn=document.querySelector('.reset');
+resetBtn.addEventListener('click',()=>{
+    alert('Page is Reloading');
+    location.reload();
+});
